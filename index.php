489295aa66f5de1cd37837276940e9baa1f1c766
furukawa1020 <f.kotaro.0530@gmail.com>
2025-11-03 12:19:15 +0900

    fix: エラーハンドリング設定をデバッグモードに変更

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
  * @copyright 2025 Solitude Victory Project
  */
 
-// エラーハンドリング設定
+// エラーハンドリング設定（デバッグモード）
 error_reporting(E_ALL);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
 ini_set('log_errors', 1);
 ini_set('error_log', __DIR__ . '/logs/error.log');
 
